9b65b6c0c0b583f153625f5b44bd6c081120baa8
nthyq <nthyq01@gmail.com>
2025-12-23 17:04:03 +0700

    EmployeePage, CustomerPage and Profile

--- frontend/src/services/authService.js
+++
@@ -1,7 +0,0 @@
-import axios from "./axios";
-
-const handleLoginApi = (TenTaiKhoan, MatKhau) => {
-  return axios.post("/api/login", { TenTaiKhoan, MatKhau });
-};
-
-export { handleLoginApi };
--- frontend/src/services/dashboardService.js
+++
@@ -1,9 +0,0 @@
-import axios from "./axios";
-const dashboardService = {
-  getStats: () => axios.get("/api/dashboard/stats"),
-  getRevenueData: () => axios.get("/api/dashboard/revenue"),
-  getCategoryData: () => axios.get("/api/dashboard/category"),
-  getOrderData: () => axios.get("/api/dashboard/orders"),
-};
-
-export default dashboardService;
