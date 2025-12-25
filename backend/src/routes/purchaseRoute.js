6addc81921cd51051f2889b2a7e2d9187b9b494d
HoangYen5 <166485073+HoangYen5@users.noreply.github.com>
2025-12-25 14:07:26 +0700

    Update purchaseRoute.js

--- backend/src/routes/purchaseRoute.js
+++ backend/src/routes/purchaseRoute.js
@@ -1,14 +1,40 @@
 import express from "express";
 import purchaseController from "../controllers/purchaseController.js";
+import verifyRole from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 const initPurchaseRoute = (app) => {
-  router.get("/api/purchases", purchaseController.getPurchases);
-  router.get("/api/purchases/:id", purchaseController.getPurchaseById);
-  router.post("/api/purchases/create", purchaseController.createPurchase);
-  router.post("/api/purchases/update", purchaseController.updatePurchase);
-  router.post("/api/purchases/delete", purchaseController.deletePurchases);
+  router.get(
+    "/api/purchases",
+    verifyRole.verifyToken,
+    verifyRole.checkPermission(["admin", "warehouse"]),
+    purchaseController.getPurchases
+  );
+  router.get(
+    "/api/purchases/:id",
+    verifyRole.verifyToken,
+    verifyRole.checkPermission(["admin", "warehouse"]),
+    purchaseController.getPurchaseById
+  );
+  router.post(
+    "/api/purchases/create",
+    verifyRole.verifyToken,
+    verifyRole.checkPermission(["admin", "warehouse"]),
+    purchaseController.createPurchase
+  );
+  router.post(
+    "/api/purchases/update",
+    verifyRole.verifyToken,
+    verifyRole.checkPermission(["admin", "warehouse"]),
+    purchaseController.updatePurchase
+  );
+  router.post(
+    "/api/purchases/delete",
+    verifyRole.verifyToken,
+    verifyRole.checkPermission(["admin"]),
+    purchaseController.deletePurchases
+  );
   return app.use("/", router);
 };
 
