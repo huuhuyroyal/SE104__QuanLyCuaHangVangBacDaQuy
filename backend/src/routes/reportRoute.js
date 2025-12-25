d62dda04fffa3a68d92ad19e922cbdaa03dc6243
HoangYen5 <166485073+HoangYen5@users.noreply.github.com>
2025-12-25 14:08:03 +0700

    Update reportRoute.js

--- backend/src/routes/reportRoute.js
+++ backend/src/routes/reportRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
 import ReportController from "../controllers/reportController.js";
+import verifyRole from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 const initReportRoute = (app) => {
-  router.get("/api/report", ReportController.getReport);
+  router.get(
+    "/api/report",
+    verifyRole.verifyToken,
+    verifyRole.checkPermission(["admin", "warehouse"]),
+    ReportController.getReport
+  );
 
   return app.use("/", router);
 };
