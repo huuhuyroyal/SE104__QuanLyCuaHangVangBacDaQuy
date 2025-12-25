a7ab6fbef4039bc2816e7c692cb956cec031f97a
HoangYen5 <166485073+HoangYen5@users.noreply.github.com>
2025-12-25 14:04:44 +0700

    Refactor SQL query in getAllProducts method

--- backend/src/models/productModel.js
+++ backend/src/models/productModel.js
@@ -5,7 +5,25 @@ const ProductModel = {
   getAllProducts: async () => {
     try {
       const query = `
-        SELECT s.*, l.TenLoaiSanPham 
+        SELECT 
+          s.MaSanPham,
+          s.TenSanPham,
+          s.HinhAnh,
+          s.MaLoaiSanPham,
+          s.isDelete,
+          s.DonGiaMuaVao, 
+          (
+            COALESCE((SELECT SUM(SoLuongMua) FROM chitietmuahang WHERE MaSanPham = s.MaSanPham), 0) - 
+            COALESCE((SELECT SUM(SoLuongBan) FROM chitietbanhang WHERE MaSanPham = s.MaSanPham), 0)
+          ) AS SoLuongTon,
+          CASE 
+            WHEN s.DonGiaMuaVao > 0 THEN 
+               ROUND(s.DonGiaMuaVao * (1 + (COALESCE(l.PhanTramLoiNhuan, 0) / 100)))
+            ELSE s.DonGiaBanRa 
+          END AS DonGiaBanRa,
+          l.TenLoaiSanPham,
+          l.PhanTramLoiNhuan
+
         FROM sanpham s 
         LEFT JOIN loaisanpham l ON s.MaLoaiSanPham = l.MaLoaiSanPham
         ORDER BY s.MaSanPham DESC
